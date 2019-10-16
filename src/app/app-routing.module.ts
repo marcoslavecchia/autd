2a9a7f3f161864ed05c7d8a306a7621eecd53b51
Marcos V M de Souza <marcosvms@gmail.com>
2019-10-16 16:45:45 -0300

    firebase and tabs setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
+  //{ path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  //{ path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
 ];
 
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
